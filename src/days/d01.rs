6eb3b0f76b2f8d9901a342e85870d9e1e3ae4e45
Élise <elise@eiss.fr>
2022-12-01 21:25:16 +0100

    solved first

--- src/days/d01.rs
+++ src/days/d01.rs
@@ -1,8 +1,25 @@
 // https://adventofcode.com/2022/day/1
 
 pub fn solve(input: String) -> (String, String) {
-    // Put your solving code here.
+    let mut greatest_total = 0;
+    let mut accumulator = 0;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            if accumulator > greatest_total {
+                greatest_total = accumulator
+            }
+            accumulator = 0;
+        }
+
+        match i32::from_str_radix(line, 10) {
+            Ok(current) => {
+                accumulator += current;
+            }
+            Err(_) => {}
+        }
+    }
 
     // Return the solution to part1 and part2 in a tuple.
-    ("".into(), "".into())
+    (greatest_total.to_string(), "".into())
 }
