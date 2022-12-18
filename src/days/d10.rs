4670f67ed789b16bbb4554e57fa373ca3deddb32
Élise <elise@eiss.fr>
2022-12-18 23:51:46 +0100

    this is a mess

--- src/days/d10.rs
+++ src/days/d10.rs
@@ -1,16 +1,16 @@
 // https://adventofcode.com/2022/day/10
 
 enum Instruction {
-    Addx(u32),
+    Addx(i32),
     Noop,
 }
 
-const REPORT_AT: [u32; 6] = [20, 60, 100, 140, 180, 220];
+const REPORT_AT: [i32; 6] = [20, 60, 100, 140, 180, 220];
 
 struct Reporter<'a> {
-    next_report_iter: std::slice::Iter<'a, u32>,
-    next_report: u32,
-    accumulator: u32,
+    next_report_iter: std::slice::Iter<'a, i32>,
+    next_report: i32,
+    accumulator: i32,
 }
 
 impl<'a> Reporter<'a> {
@@ -26,9 +26,9 @@ impl<'a> Reporter<'a> {
             accumulator: 0,
         }
     }
-    pub fn report(&mut self, tick: u32, register: u32) {
+    pub fn report(&mut self, tick: i32, register: i32) {
         if tick == self.next_report {
-            self.accumulator += register;
+            self.accumulator += register * tick;
             self.next_report = match self.next_report_iter.next() {
                 Some(val) => *val,
                 None => 0,
@@ -38,30 +38,51 @@ impl<'a> Reporter<'a> {
 }
 
 struct State<'a, 'b> {
-    register: u32,
-    tick: u32,
+    register: i32,
+    tick: i32,
     instruction: Option<Instruction>,
     sleep_cycles: u32,
     reporter: Reporter<'a>,
     commands: std::str::Lines<'b>,
     done: bool,
+    screen_line: String,
+    sprite_position: usize,
 }
 
 impl<'a, 'b> State<'a, 'b> {
     pub fn new(commands: std::str::Lines<'b>) -> Self {
+        let screen = ".".repeat(40);
         Self {
-            register: 0,
+            register: 1,
             tick: 0,
             instruction: None,
             sleep_cycles: 0,
             reporter: Reporter::new(),
             commands,
             done: false,
+            screen_line: screen,
+            sprite_position: 0,
         }
     }
 
-    pub fn signal_strengh(&self) -> u32 {
-        self.register * self.tick
+    pub fn reset_line(&mut self) {
+        self.screen_line = ".".repeat(40);
+    }
+
+    pub fn write_on_screen_line(&mut self) {
+        match usize::try_from((self.tick % 40) - 1) {
+            Ok(px_idx) if px_idx < self.screen_line.len() - 1 => {
+                let replace_with =
+                    if px_idx >= self.sprite_position && px_idx < self.sprite_position + 3 {
+                        "#"
+                    } else {
+                        "."
+                    };
+                self.screen_line
+                    .replace_range(px_idx..px_idx + 1, replace_with);
+            }
+            _ => {}
+        }
     }
 
     pub fn process_line(&mut self, line: &str) {
@@ -69,7 +90,7 @@ impl<'a, 'b> State<'a, 'b> {
             self.instruction = Some(Instruction::Noop);
             self.sleep_cycles = 1;
         } else if let Some(val) = line.strip_prefix("addx ") {
-            self.instruction = Some(Instruction::Addx(match u32::from_str_radix(val, 10) {
+            self.instruction = Some(Instruction::Addx(match val.parse() {
                 Ok(val) => val,
                 _ => 0,
             }));
@@ -81,25 +102,43 @@ impl<'a, 'b> State<'a, 'b> {
 
     pub fn next_cycle(&mut self) {
         self.tick += 1;
-        if self.sleep_cycles != 0 {
-            self.sleep_cycles -= 1;
-        } else {
-            match self.instruction {
-                Some(Instruction::Addx(value)) => {
-                    self.register += value;
-                    self.instruction = None;
-                }
-                Some(Instruction::Noop) => {
-                    self.instruction = None;
-                }
-                _ => {}
-            }
-            match self.commands.next() {
+
+        if self.tick != 1 && self.tick % 40 == 1 {
+            println!("{}", self.screen_line);
+            self.reset_line();
+            self.sprite_position = 0;
+        }
+        self.write_on_screen_line();
+
+        match self.instruction {
+            None => match self.commands.next() {
                 Some(command) => self.process_line(command),
                 None => self.done = true,
+            },
+            _ => {}
+        }
+
+        self.reporter.report(self.tick, self.register);
+
+        if self.sleep_cycles != 0 {
+            self.sleep_cycles -= 1;
+            if self.sleep_cycles == 0 {
+                match self.instruction {
+                    Some(Instruction::Addx(val)) => {
+                        self.instruction = None;
+                        self.register += val;
+                        self.sprite_position = match usize::try_from(self.register) {
+                            Ok(val) => val,
+                            Err(_) => 1,
+                        } - 1;
+                    }
+                    Some(Instruction::Noop) => {
+                        self.instruction = None;
+                    }
+                    _ => {}
+                }
             }
         }
-        self.reporter.report(self.tick, self.register);
     }
 
     pub fn run(&mut self) {
